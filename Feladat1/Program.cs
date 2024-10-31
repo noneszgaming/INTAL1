395f5eb6c39056e904c632a921391ddde43f2bd6
YoungLee12 <68099807+YoungLee12@users.noreply.github.com>
2024-10-31 11:45:27 +0100

    Add files via upload

---
+++ Feladat1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char betu = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            string szoveg = Console.ReadLine();
+
+            if (szoveg.Length > 0)
+            {
+                if (szoveg.Contains(betu))
+                {
+                    Console.WriteLine(szoveg.IndexOf(betu));
+                }
+            }
+        }
+    }
+}
