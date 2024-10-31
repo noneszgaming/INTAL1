865fe944a1767da49d8d5d638c2e9866440ec180
YoungLee12 <68099807+YoungLee12@users.noreply.github.com>
2024-10-31 11:47:13 +0100

    Add files via upload

---
+++ Feladat3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tomb = [4, 3, 10, 1, 1];
+            int szam = 1;
+
+            Console.WriteLine(LinearisKereses(tomb, szam));
+        }
+
+        static bool LinearisKereses(int[] tomb, int szam)
+        {
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i] == szam)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
