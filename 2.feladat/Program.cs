9c08fae206e47871cf9fa7ba540b5db14c62db0e
ivszabolcs <102289954+ivszabolcs@users.noreply.github.com>
2024-10-31 14:04:26 +0100

    2.feladat

---
+++ 2.feladat/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PeldaHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Írj be egy szöveget: ");
+            String beker = Console.ReadLine();
+            char[] arr = beker.ToCharArray();
+            Array.Sort(arr);
+            Console.WriteLine(arr);
+            Console.ReadKey();
+        }
+    }
+}
