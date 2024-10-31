55cc0c1359b439149add36e4bd54db56e37b9a8b
HUNKMark2062 <kmark2062@gmail.com>
2024-10-31 14:35:55 +0100

    Feladatok 20241031 uploaded.

---
+++ K_Mark/20241031_ora/Feladat01/Feladat01/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             
+            Console.WriteLine("Enter string character:");
+
+            string inputCharacter = Console.ReadLine();
+
+            //
+            //string s = "9quali52ty3";
+            byte[] ASCIIValues = Encoding.ASCII.GetBytes(inputCharacter);
+            foreach (byte b in ASCIIValues)
+            {
+                Console.WriteLine(b);
+            }
+
+            //Console.WriteLine("inputCharacter is: " + inputCharacter);
+
+            */
+
+
+            // 1. feladat megoldása
+
+            Console.WriteLine("Enter a string character:");
+
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine("Enter a char to search for:");
+
+            string inputCharacterRaw = Console.ReadLine();
+
+            char inputCharacter = char.Parse(inputCharacterRaw);
+
+
+            //Console.WriteLine(inputString);
+
+            //Console.WriteLine(inputCharacter);
+
+
+            if (inputString.Contains(inputCharacter))
+            {
+                Console.WriteLine(inputString);
+
+                int searchResult = inputString.IndexOf(inputCharacter);
+
+                Console.WriteLine(searchResult + ". index");
+
+            }
+            else
+            {
+                Console.WriteLine("Not Found.");
+
+            }
+
+
+        }
+    }
+}
+
---
+++ K_Mark/20241031_ora/Feladat02/Feladat02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a string:");
+
+            string inputMyString = Console.ReadLine();
+
+            //Console.WriteLine("Username is: " + userName);
+
+            //Console.WriteLine(inputMyString);
+            Console.WriteLine();
+
+
+            inputMyString = String.Concat(
+                inputMyString
+                .OrderBy(c => char.ToUpper(c))
+                .ThenBy(c => c)
+            );
+
+            //string sortedStr = inputMyString.OrderBy(myChar => myChar).ToString();
+
+            Console.WriteLine(inputMyString);
+
+
+        }
+    }
+}
