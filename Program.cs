c4d3b69135c9f8a05b31338f08a38f6f2bfb5b33
FyodorJK <nyarfafasora@gmail.com>
2024-11-13 12:55:02 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PeldaHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Írj be egy szöveget: ");
+            int bek = Convert.ToInt32(Console.ReadLine());
+            int[] tomb = { 1, 2, 3, 4, 5, 6, 7};
+            bool talalt = false;
+            for(int i = 0; i < 5; i++)
+            {
+                if (tomb[i] == bek)
+                {
+                    bek = tomb[i];
+                    Console.Write("Megvan a(z)" + bek + " a(z) " + (i + 1) + ". volt");
+                    talalt = true;
+                    break;
+                }                Console.WriteLine(i);
+            }
+            if (!talalt)
+            {
+                Console.Write("Nem találta meg a(z): " + bek);
+            }
+
+        }
+    }
+}
