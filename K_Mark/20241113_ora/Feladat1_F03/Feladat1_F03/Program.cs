9b217d6d6f414c1352d6da9224a5f10254b27513
HUNKMark2062 <kmark2062@gmail.com>
2024-11-13 14:18:41 +0100

    Feladatok 20241031 uploaded.

---
+++ K_Mark/20241113_ora/Feladat1_F03/Feladat1_F03/Program.cs
@@ -0,0 +1,57 @@
+﻿// Feladat 1
+// 3. Lineáris keresés (rendezetlen és rendezett esetben)
+//
+//
+// RENDEZETLEN eset
+//
+//
+
+
+using System;
+using System.Text;
+
+namespace Feladat1_F03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] nums = { 121231, 52, 8565, 422 };
+
+            int searchedNumber = 8565;        //searchedNumber = 52;
+
+            int iteratedIndex = 0;
+
+            foreach (int iter in nums)
+            {
+
+                //Console.WriteLine(iter);
+
+                //Console.WriteLine(iter);
+
+                if (iter == searchedNumber)
+                {
+                    //Console.WriteLine("Az elem helye:", iter, "maga az elem:", iter);
+
+                    Console.WriteLine("az elem: " + iter);
+                    Console.WriteLine("index: " + iteratedIndex);
+
+                }
+                else
+                {
+
+                    iteratedIndex++;
+
+                    //Console.WriteLine("Nincs ilyen elem.");
+
+
+                }
+
+            }
+
+        }
+    }
+
+}
---
+++ K_Mark/20241113_ora/Feladat3_F05/Feladat3_F05/Program.cs
@@ -0,0 +1,61 @@
+﻿// Feladat 3
+//
+// 5. Láncolt lista. 'CRUD'
+//
+// Linked List : create , read , update , delete
+//
+//
+
+
+using System;
+using System.Collections.Generic;
+
+class Feladat3_F05
+{
+
+    static public void Main()
+    {
+
+        LinkedList<String> my_list = new LinkedList<String>();
+
+
+        // create
+        my_list.AddLast("Adam");
+        my_list.AddLast("Bela");
+        my_list.AddLast("Cecil");
+        my_list.AddLast("Dominika");
+
+
+        Console.WriteLine("Best students of XYZ university:");
+
+
+        // read
+        foreach (string str in my_list)
+        {
+            Console.WriteLine(str);
+        }
+
+
+        Console.WriteLine();
+        // update
+        my_list.Find("Dominika").Value = "Domi";
+
+        foreach (string str in my_list)
+        {
+            Console.WriteLine(str);
+        }
+
+
+        Console.WriteLine();
+        // delete
+        my_list.Remove("Cecil");
+
+        foreach (string str in my_list)
+        {
+            Console.WriteLine(str);
+        }
+
+    }
+
+
+}
