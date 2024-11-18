fb732c6f06f4324334d3ee00e576277c84546439
YoungLee12 <68099807+YoungLee12@users.noreply.github.com>
2024-11-18 23:37:26 +0100

    Add files via upload

---
+++ gyakorlo/Nakazawa Leó/SprintDemo/SprintDemo/Sprint.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SprintDemo
+{
+    public class Sprint : DemoControl
+    {
+        public Sprint(Size size, Point location, string description, Font font, Control parent) : base(size, location, description, font, parent)
+        {
+            Label.BorderStyle = BorderStyle.FixedSingle;
+            Label.TextAlign = ContentAlignment.BottomCenter;
+        }
+    }
+}
---
+++ gyakorlo/Nakazawa Leó/SprintDemo/SprintDemo/Task.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SprintDemo
+{
+    public class Task : DemoControl
+    {
+        public Point OriginalLocation { get; }
+
+        public Task(Size size, Point location, string description, Font font, Control parent) : base(size, location, description, font, parent)
+        {
+            OriginalLocation = location;
+            Label.TextAlign = ContentAlignment.MiddleCenter;
+        }
+    }
+}
