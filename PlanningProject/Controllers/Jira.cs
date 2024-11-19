96adbb286093babff69c766cb07b06ce26e4f41f
mihalyroli <mihalyrolcsi@gmail.com>
2024-11-19 15:08:37 +0100

    Create Endpoint data

--- PlanningProject/Controllers/Jira.cs
+++ PlanningProject/Controllers/Jira.cs
@@ -11,16 +11,27 @@ namespace PlanningProject.Controllers
     public class Jira : ControllerBase
     {
         private readonly HttpClient _httpClient;
+        private const string JIRA_BASE_URL = "https://planningproject2024.atlassian.net/rest/api/3/issue";
 
         public Jira(HttpClient httpClient)
         {
             _httpClient = httpClient;
-            var byteArray = System.Text.Encoding.ASCII.GetBytes("planningproject2024@gmail.com:ATATT3xFfGF01NMkKpNj4DUNV8KyPb0GWXKwAa1jtjrucR22c_lroCV5iEW7tscQcnZhwB4MhefjVVMdVlxPeJAWEl5egbX9MZxL211zsv9XX5Z0ambRpIKfGLK0-3-mRNCc6dLi0Dlh88zb9zlHfXoZCIqc5m8YP_Oe2xhkAl8ApAxzWRhXbY0=8835AC2B");
+            var byteArray = System.Text.Encoding.ASCII.GetBytes("planningproject2024@gmail.com:ATATT3xFfGF0SLCL4yoivp1vtRSjY17hW8oVmrb9O9V5K8-PwkDoSPk4Cp2nc9QY0gVKB2aHRQLAJDoqSGKVGK3JXpV22We5GkIVRAssE8dB5poklnaO9HA8wCeBDTKqVySW4CiZg7tQZvAROb-6UwI-78Gkkcf0Ww_BDHLwU5WbC-puYoAwz6M=D9321DFA");
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+            _httpClient.BaseAddress = new Uri(JIRA_BASE_URL);
         }
 
-  
+        [HttpGet("task/{issueKey}/{storyPoints}")]
+        public async Task<IActionResult> UpdateStoryPoints(string issueKey, int storyPoints)
+        {
+            var updateData = new
+            {
+                fields = new
+                {
+                    customfield_10016 = storyPoints
+                }
+            };
 
-        // Other actions can be similarly updated to handle Jira data
+        }
     }
 }
