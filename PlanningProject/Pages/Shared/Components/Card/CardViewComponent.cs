3d7cd12b5a8941a994d22094bd918203bb19fc9c
Ocskó Bianka - KX38XT <ocsko.bianka@gmail.com>
2024-11-19 16:40:53 +0100

    Card component, not working

---
+++ PlanningProject/Pages/Shared/Components/Card/CardViewComponent.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlanningProject.Pages.Shared.Components
+{
+    public class CardViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(bool isVoted, bool isSelected)
+        {
+            var model = new CardViewModel 
+            { 
+                isVoted = isVoted,
+                isSelected = isSelected
+            };
+            return View(isVoted);
+        }
+    }
+}
---
+++ PlanningProject/Pages/Shared/Components/Card/ViewModel.cs
@@ -0,0 +1,5 @@
+public class CardViewModel
+{
+    public bool isVoted { get; set; }
+    public bool isSelected { get; set; }
+}
