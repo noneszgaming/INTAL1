9e8960b94378b1967aa3cc0f454a2c05ddfcef70
Levente Nagy <nagylevi0503@gmail.com>
2024-11-19 15:27:27 +0100

    Title component added, Titon One font added

--- PlanningProject/tailwind.config.js
+++ PlanningProject/tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: { 
+        'titan': ['"Titan One"', 'cursive'],
+      },
       colors: {
         'cstm-purple': '#9B00F4',
       },
