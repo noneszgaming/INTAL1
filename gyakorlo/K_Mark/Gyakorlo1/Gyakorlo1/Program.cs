75dac1d62915d804ab213fb24b506e93582e249f
HUNKMark2062 <kmark2062@gmail.com>
2024-11-19 15:28:06 +0100

    Gyakorlo1 project uploaded

---
+++ gyakorlo/K_Mark/Gyakorlo1/Gyakorlo1/Program.cs
@@ -0,0 +1,46 @@
+﻿//  Demo Feladat:	
+//
+//      Input username adat eltárol változóba, 
+//	    ezután txt -be kiírattat.
+
+using System;
+
+namespace Gyakorlo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter username: ");
+
+            string userName = Console.ReadLine();
+
+            //Console.WriteLine("Username is: " + "'" + userName + "'");
+
+
+            // writing into a txt file 
+            var outputPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\outputText.txt"));
+
+
+            var outputText = File.ReadAllText(outputPath);
+
+            var outputLines = File.ReadAllLines(outputPath);
+
+
+            using (StreamWriter outputFile = new StreamWriter(outputPath, append: true)) 
+            {
+
+                outputFile.WriteLine(userName);
+            
+            }
+
+            Console.WriteLine("\nWrite to file finished.");
+
+        }
+
+
+    }
+
+
+}
