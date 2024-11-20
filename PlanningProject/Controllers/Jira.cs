a3ca936a488ebbc07a253c50db175068209303a4
Szilagyi Zoltan <noneszgaming@gmail.com>
2024-11-20 15:04:57 +0100

    sprint model put env variables small changes

--- PlanningProject/Controllers/Jira.cs
+++ PlanningProject/Controllers/Jira.cs
@@ -11,12 +11,17 @@ namespace PlanningProject.Controllers
     public class Jira : ControllerBase
     {
         private readonly HttpClient _httpClient;
-        private const string JIRA_BASE_URL = "https://planningproject2024.atlassian.net/rest/api/3/issue";
+        private const string JIRA_BASE_URL = "https://planningproject2024.atlassian.net/rest/";
 
         public Jira(HttpClient httpClient)
         {
+
+            var email = Environment.GetEnvironmentVariable("jira_email");
+            var api = Environment.GetEnvironmentVariable("jira_api_key");
+            
+            
             _httpClient = httpClient;
-            var byteArray = System.Text.Encoding.ASCII.GetBytes("planningproject2024@gmail.com:ATATT3xFfGF0w_vC5oOrtDH9hdx1z97kXNmq3UNyNnuVQAIdJPr7BATWPA8vaO4wpYGabMzKeBFAtV1F48NnQDosb8RkYXrl0gNYqQKNSxcwubg29fmFPiMYAbjAaijSHsHdxLG6E0KbEXaMi93V12XOfaj0QLDA41xU6D_tb85w9uEcYlZRDyg=F31CD981");
+            var byteArray = System.Text.Encoding.ASCII.GetBytes($"{email}:{api}");
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
             _httpClient.BaseAddress = new Uri(JIRA_BASE_URL);
         }
@@ -31,7 +36,7 @@ namespace PlanningProject.Controllers
                     customfield_10016 = storyPoints
                 }
             };
-            var response = await _httpClient.PutAsJsonAsync($"{JIRA_BASE_URL}/{issueKey}", updateData);
+            var response = await _httpClient.PutAsJsonAsync($"{JIRA_BASE_URL}api/3/issue/{issueKey}", updateData);
             if (response.IsSuccessStatusCode)
             {
                 return Ok($"Story points updated for {issueKey} to {storyPoints}");
@@ -43,7 +48,7 @@ namespace PlanningProject.Controllers
         [HttpGet("tasks/{customValue}")]
         public async Task<IActionResult> GetTasks(string customValue)
         {
-            var response = await _httpClient.GetAsync($"https://planningproject2024.atlassian.net/rest/agile/1.0/sprint/{customValue}/issue");
+            var response = await _httpClient.GetAsync($"{JIRA_BASE_URL}agile/1.0/sprint/{customValue}/issue");
             if (response.IsSuccessStatusCode)
             {
                 var data = await response.Content.ReadAsStringAsync();
@@ -56,7 +61,7 @@ namespace PlanningProject.Controllers
         [HttpGet("sprints")]
         public async Task<IActionResult> GetSprints()
         {
-            var response = await _httpClient.GetAsync("https://planningproject2024.atlassian.net/rest/agile/1.0/board/3/sprint");
+            var response = await _httpClient.GetAsync($"{JIRA_BASE_URL}agile/1.0/board/3/sprint");
             if (response.IsSuccessStatusCode)
             {
                 var data = await response.Content.ReadAsStringAsync();
