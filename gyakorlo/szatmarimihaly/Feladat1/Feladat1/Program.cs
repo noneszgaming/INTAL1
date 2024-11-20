17c171e9cff134a0a10f50bfea817314ec92153d
HUNKMark2062 <kmark2062@gmail.com>
2024-11-20 13:04:24 +0100

    Files from szatmarimihaly uploaded.

---
+++ gyakorlo/szatmarimihaly/Feladat1/Feladat1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Feladat Átlag számítás
+
+
+using System;
+
+namespace Feladat1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] szamLista = { 19, 21, 21, 19, 21, 21, 18, 25, 20, 19, 16 };
+
+            int szamListaHossz = szamLista.Length;
+
+            int sum = 0;
+
+            for (int i = 0; i < szamListaHossz; i++)
+            {
+
+                sum += szamLista[i];
+
+            }
+
+            double atlag = sum / szamListaHossz;
+
+            Console.WriteLine(atlag);
+
+        }
+    }
+}
+
---
+++ gyakorlo/szatmarimihaly/Feladat2/Feladat2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Feladat Módusz kiszámítása
+
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+
+        int[] szamLista = { 19, 21, 21, 19, 21, 21, 18, 25, 20, 19, 16 };
+
+        int szamListaHossz = szamLista.Length;
+
+
+        Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
+        int legtobbDarabszamValaha = 0;
+        int vegsoModuszErtek = -1;
+
+
+        for (int k = 0; k < szamListaHossz; k++)
+        {
+            int jelenlegiSzam = szamLista[k];
+            if (frequencyMap.ContainsKey(jelenlegiSzam))
+            {
+
+                frequencyMap[jelenlegiSzam]++;
+
+            }
+            else
+            {
+                frequencyMap[jelenlegiSzam] = 1;
+            }
+
+
+            if (frequencyMap[jelenlegiSzam] > legtobbDarabszamValaha)
+            {
+                legtobbDarabszamValaha = frequencyMap[jelenlegiSzam];
+                vegsoModuszErtek = jelenlegiSzam;
+            }
+        }
+
+        Console.WriteLine(vegsoModuszErtek + ", " + legtobbDarabszamValaha + " times");
+    }
+}
+
