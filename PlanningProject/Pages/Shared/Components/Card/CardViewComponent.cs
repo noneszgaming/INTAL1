017d67a98fd4756ada53a7c60436f7632b759528
Levente Nagy <nagylevi0503@gmail.com>
2024-11-22 16:02:25 +0100

    Card component optimalized

--- PlanningProject/Pages/Shared/Components/Card/CardViewComponent.cs
+++ PlanningProject/Pages/Shared/Components/Card/CardViewComponent.cs
@@ -4,12 +4,11 @@ namespace PlanningProject.Pages.Shared.Components
 {
     public class CardViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke(bool isVoted, bool isSelected, bool isRevealed, int value)
+        public IViewComponentResult Invoke(bool isVoted, bool isRevealed, int value)
         {
             var model = new CardViewModel
             {
                 isVoted = isVoted,
-                isSelected = isSelected,
                 isRevealed = isRevealed,
                 value = value
             };
--- PlanningProject/Pages/Shared/Components/Card/ViewModel.cs
+++ PlanningProject/Pages/Shared/Components/Card/ViewModel.cs
@@ -1,10 +1,8 @@
 public class CardViewModel
 {
     public bool isVoted { get; set; }
-    // lila-szürke kártya szám nélkül
-    public bool isSelected { get; set; }
-    // lila kártya számmal
+
     public bool isRevealed { get; set; }
-    // fehér kártya számmal
+
     public int value { get; set; }
 }
