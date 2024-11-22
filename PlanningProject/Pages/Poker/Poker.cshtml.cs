b77cb5ea50ef7f91881878be67277c4e13bb5214
Levente Nagy <nagylevi0503@gmail.com>
2024-11-22 15:34:26 +0100

    CardDeck added Card updated _Layout updated with a bg

---
+++ PlanningProject/Pages/Poker/Poker.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PlanningProject.Pages.Poker
+{
+    public class PokerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
