63d824f63dbc030f42d670272f26f8a201879187
szabolcsarato <arato6zoli@gmail.com>
2024-11-27 16:33:20 +0100

    Micro Model

---
+++ JiraMicroservice/Models/Task.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.HttpLogging;
+using Newtonsoft.Json;
+using PlanningProject.Models;
+
+namespace PlanningProject.Models
+{ 
+    public class IssueResponse
+    {
+        public string Expand { get; set; }
+        public int StartAt {  get; set; }
+        public int MaxResults { get; set; }
+        public int Total { get; set; }
+        public List<Issue> Issues { get; set; }
+    }
+
+    public class Issue
+    {
+        public string Key { get; set; }
+        public Fields Fields { get; set; }
+    }
+
+    public class Fields
+    {
+        [JsonProperty("customfield_10016")]
+        public double? StoryPoints { get; set; }
+        public string Description { get; set; }
+        public Sprint Sprint { get; set; }
+
+
+    }
+}
